81f02a079861e9003f640e9dcf776b29a58e52d8
Jersson Moros <jersson0423@gmail.com>
2023-03-13 17:31:11 -0500

    Se practica la edicion de archivos txt con libreria nativa

---
+++ Comienzos en python/gestionArchivos.py
@@ -0,0 +1,13 @@
+import os
+
+# c = open('Comienzos en python\\archivoTexto.txt')
+
+# print(c.readline())
+
+# c.close()
+
+if os.path.exists('Comienzos en python\\archivoTexto.txt'):
+    
+    os.remove('Comienzos en python\\archivoTexto.txt')
+
+os.rmdir()
