196544b6a181a720ac14e5ca234bd84a05879483
Jersson Moros <jersson0423@gmail.com>
2023-03-13 17:30:21 -0500

    Se hacen los ejercicios del curso de Udemy, conocimientos generales

---
+++ Comienzos en python/Ejercicios/EjerciciosdelCurso.py
@@ -0,0 +1,42 @@
+# num1 = input('digite el primer numero')
+# num2 = input('Digite otra numero')
+# total = 0
+
+# for i in range(int(num2)):
+#     total += int(num1)
+
+# print(total)
+
+# nombre = input('Digite el nombre')
+
+# for i in nombre[::-1]:
+#     print(i, end='')
+
+# import random
+# lista = []
+
+# for i in range(15):
+#     lista.append(random.randint(-5,20))
+
+# print(lista)
+
+# menor = lista[0]
+
+# for i in lista:
+#     menor = i if i < menor else menor
+
+# print('El numero menor de la lista es: {}'.format(menor))
+
+# import math
+# def volumenEsfera(radio):
+#     total = 4/3 * math.pi * radio ** 3
+#     return total
+
+# radio = input('Digite el radiod e la esfera: ')
+# try:
+#     radio = float(radio)
+# except:
+#     print('No es un numero')
+
+
+# print('el radio de la esfera es:', volumenEsfera(radio))
