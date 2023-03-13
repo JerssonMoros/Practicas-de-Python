27c9adb421476c7ad1d4b629e713c731c22fb3d8
Jersson Moros <jersson0423@gmail.com>
2023-03-13 12:16:55 -0500

    Actualizacion de ejercicios sobre clases

--- Comienzos en python/Clases&Objetos/Ejercicios.py
+++ Comienzos en python/Clases&Objetos/Ejercicios.py
@@ -1,5 +1,11 @@
 class Persona():
-    def __init__(self, nombre, edad, ) -> None:
+    def __init__(self, nombre, edad, dni) -> None:
         self.nombre = nombre
         self.edad = edad
         self.dni = dni
+
+    def mostrar(self):
+        print(f'Nombre: {self.nombre}, \n Edad: {self.edad}, \n DNI: {self.dni}')
+
+    def esMayorDeEdad(self):
+        return True if self.edad else False
